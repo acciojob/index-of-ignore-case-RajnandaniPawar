ad2ec005ea2f1eddc7b194a10552bc03866ad2f4
RajnandaniPawar <164873503+RajnandaniPawar@users.noreply.github.com>
2024-10-17 23:29:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
+	  lower_str = str.lower()
+    lower_subStr = subStr.lower()
+
+    // # Use the built-in find method which returns the index of the first occurrence
+    index = lower_str.find(lower_subStr)
+
+    // # Return the index (or -1 if not found)
+    return index
+
 }
 
 // Please do not change the code below
